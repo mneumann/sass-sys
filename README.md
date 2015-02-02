5c3c69f24e431f351b2f83909f6a0f7235e96d94
winding-lines <github@seritan.net>
2015-02-01 20:29:34 -0800

    Actually link the library

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ libsass-sys
 ===========
 
 Low level Rust wrapper for the C/C++ libsass.
+
+You need to have libsass.a built locally as per the path in build.rs.
--- build.rs
+++ build.rs
@@ -1,14 +1,9 @@
 extern crate "pkg-config" as pkg_config;
 
 fn main() {
-    match pkg_config::find_library("libsass1") {
+    match pkg_config::find_library("sass") {
         Ok(_) => return,
-        Err(_) => {
-            if cfg!(windows) {
-                println!("cargo:rustc-flags=-l libsass:dylib");
-            } else {
-                println!("cargo:rustc-flags=-l libsaas:dylib");
-            }
-        }
+        Err(_) => {}
     }
+    println!("cargo:rustc-flags=-l sass -L ../../sass/libsass/lib/");
 }
