35537edb09419255bf3c920dafc8238b2332a35e
winding-lines <github@seritan.net>
2015-03-07 09:24:30 -0800

    Bundle sources of libsass as a git submodule

--- README.md
+++ README.md
@@ -2,11 +2,10 @@ libsass-sys
 ===========
 Low level Rust wrapper for the C/C++ libsass.
 
-[Travis](https://travis-ci.org/compass-rs/sass-sys) ![Travis build status]
+[Travis build status:] (https://travis-ci.org/compass-rs/sass-sys) ![Travis build status]
 (https://travis-ci.org/compass-rs/sass-sys.svg?branch=master)
 
 Documentation: http://compass-rs.github.io/sass-sys/
 
 
-
-You need to have libsass.a built locally as per the path in build.rs.
+This project builds a version of libsass from sources.
--- build.rs
+++ build.rs
@@ -1,9 +1,28 @@
 extern crate "pkg-config" as pkg_config;
+use std::os;
+use std::old_io::{self, fs, Command};
+use std::old_io::process::InheritFd;
 
 fn main() {
     match pkg_config::find_library("sass") {
         Ok(_) => return,
         Err(_) => {}
     }
-    println!("cargo:rustc-flags=-l sass -L ../../sass/libsass/lib/ -l c++");
+    let src = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap());
+    let mut cmd = Command::new("make");
+    cmd.cwd(&src.join("libsass"));
+    run(& mut cmd);
+  
+    println!("cargo:rustc-flags=-l sass -L libsass/lib/ -l c++");
+
+}
+
+fn run(cmd: &mut Command) {
+    println!("running: {:?}", cmd);
+    assert!(cmd.stdout(InheritFd(1))
+               .stderr(InheritFd(2))
+               .status()
+               .unwrap()
+               .success());
+
 }
