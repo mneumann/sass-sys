4d9d61b2a7832e098b721627b10963614f835533
Andrew Brinker <me@andrewbrinker.com>
2016-01-03 18:57:46 -0500

    Small cleanup

--- build.rs
+++ build.rs
@@ -2,47 +2,54 @@ extern crate pkg_config;
 
 use std::env;
 use std::fs;
-use std::path::{Path};
-use std::process::{Command};
+use std::path::Path;
+use std::process::Command;
 use std::io::{self,Write};
 
 static ARCHIVE: &'static str = "libsass.a";
 static PROJECT: &'static str = "libsass";
 
 fn main() {
+    // See if sass is already setup
     match pkg_config::find_library("sass") {
         Ok(_) => return,
         Err(_) => {}
     }
-    let mut stderr = io::stderr();
-    let src = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join(PROJECT);
+
+    // Setup some paths
+    let manifest = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let src = Path::new(&manifest).join(PROJECT);
     let archive = src.join("lib").join(ARCHIVE);
-    // writeln!(&mut stderr,"looking for {}", archive.display()).unwrap();
+
+    // Run make on libsass
     if !fs::metadata(archive.as_path()).is_ok() {
         let mut make = Command::new("make");
         make.current_dir(&src);
-        // writeln!(&mut stderr,"running: {:?}", make).unwrap();
         let _ = make.status().unwrap();
     }
-    // writeln!(&mut stderr, "validating that archive exists").unwrap();
-    assert!(fs::metadata(archive.as_path()).is_ok(), "Error: archive does not exist after build");
 
-    // copy to the output folder
+    // Verify that libsass was build correctly
+    assert!(fs::metadata(archive.as_path()).is_ok(),
+            "Error: archive does not exist after build");
+
+    // Setup output directory
     let out = &env::var("OUT_DIR").unwrap();
     let dst = Path::new(out);
-    // writeln!(&mut stderr, "creating {}",dst.display()).unwrap();
     let _ = fs::create_dir_all(&dst).unwrap();
+
+    // Copy archive to output directory
     match fs::copy(&archive, &dst.join(ARCHIVE)) {
         Ok(_) => {},
         Err(a) => {
+            let mut stderr = io::stderr();
             writeln!(&mut stderr,
-                        "Error {:?} when copying \n{} \nto {}", a,
-                        archive.display(), dst.display()).unwrap();
+                     "Error {:?} when copying {} to {}",
+                     a, archive.display(), dst.display()).unwrap();
             panic!("copy failed");
-            }
+        }
     }
 
-
-    println!("cargo:rustc-flags=-L native={} -l static=sass -l dylib=stdc++",dst.display());
-
+    // Link to libsass
+    println!("cargo:rustc-flags=-L native={} -l static=sass -l dylib=stdc++",
+             dst.display());
 }
